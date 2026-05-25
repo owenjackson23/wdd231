b1fefc88acc2af87cb553cdc425e2b267cff4177
owenjackson23 <owenryanjackson@icloud.com>
2026-05-25 00:57:39 -0700

    finished css for small screens

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,6 +10,7 @@ async function getMemberData() {
 
 const displayMembers = (members) => {
     members.forEach((member) => {
+        // Create elements for the card
         let card = document.createElement('section');
         let name = document.createElement('h2');
         let address1 = document.createElement('p');
@@ -19,6 +20,7 @@ const displayMembers = (members) => {
         let level = document.createElement('p');
         let logo = document.createElement('img');
 
+        // Set attributes and content
         logo.setAttribute('src', member.logo);
         logo.setAttribute('alt', `${member.name} Logo`);
         logo.setAttribute('loading', 'lazy');
@@ -26,8 +28,11 @@ const displayMembers = (members) => {
         logo.setAttribute('height', 'auto');
         name.innerHTML = member.name;
         address1.innerHTML = member.address1;
+        address1.classList.add('address');
         address2.innerHTML = member.address2;
+        address2.classList.add('address');
         phone.innerHTML = member.phone;
+        phone.classList.add('phone');
         website.setAttribute('href', member.website);
         website.innerHTML = `Visit Website`;
 
@@ -41,7 +46,7 @@ const displayMembers = (members) => {
             level.innerHTML = "Membership: Member";
         }
 
-
+        // Append elements to the card
         card.appendChild(logo);
         card.appendChild(name);
         card.appendChild(address1);
@@ -50,6 +55,9 @@ const displayMembers = (members) => {
         card.appendChild(level);
         card.appendChild(website);
 
+        card.classList.add('card');
+
+        // Append the card to the cards container
         cards.appendChild(card);
     });
 }
---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,18 @@
+const headerNav = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+
+// Toggle the navigation menu when the hamburger button is clicked
+hamButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('menu click before', headerNav.className, hamButton.className);
+    headerNav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+    console.log('menu click after', headerNav.className, hamButton.className);
+
+    const isOpen = hamButton.classList.contains("show");
+    // Changes the aria label for the hamButton
+    hamButton.setAttribute(
+        "aria-label",
+        isOpen ? "Close navigation menu" : "Open navigation menu"
+    );
+});
