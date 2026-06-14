7409f1f8069c829a7729574a11bba8da476ac2c0
owenjackson23 <owenryanjackson@icloud.com>
2026-06-14 15:36:57 -0700

    created poi-data

---
+++ chamber/data/poi-data.mjs
@@ -0,0 +1,60 @@
+const pointsOfInterest = [
+    {
+        "name": "Arizona Museum of Natural History",
+        "photo": "../images/az-museum-of-natural-history.webp",
+        "address1": "53 N Macdonald,",
+        "address2": "Mesa, AZ 85201",
+        "description": "The Arizona Museum of Natural History located in Mesa, Arizona, is the only natural history museum in the greater Phoenix area. It exhibits the natural and cultural history of the Southwestern United States."
+    },
+    {
+        "name": "Canyon Lake",
+        "photo": "../images/canyon-lake.webp",
+        "address1": "16802 AZ-88,",
+        "address2": "Apache Junction, AZ 85119",
+        "description": "Canyon Lake is one of four reservoirs that were formed by the damming of the Salt River in the U.S. state of Arizona as part of the Salt River Project. It was formed by the Mormon Flat Dam, which was completed in 1925 after two years of construction."
+    },
+    {
+        "name": "Mesa Arts Center",
+        "photo": "../images/mesa-arts-center.webp",
+        "address1": "1 E. Main St.,",
+        "address2": "Mesa, AZ 85201",
+        "description": "The Mesa Arts Center is a performing and visual arts complex in downtown Mesa, Arizona. At more than 210,000 square feet square feet, the $95 million facility, completed in 2005, is the largest comprehensive arts campus in the state."
+    },
+    {
+        "name": "Organ Stop Pizza",
+        "photo": "../images/organ-stop-pizza.webp",
+        "address1": "1149 E Southern Ave.,",
+        "address2": "Mesa, AZ 85204",
+        "description": "Organ Stop Pizza is a pizza restaurant that boasts live entertainment performed on a massive organ."
+    },
+    {
+        "name": "Riverview Park",
+        "photo": "../images/riverview-park.webp",
+        "address1": "2100 W Rio Salado Pkwy,",
+        "address2": "Mesa, AZ 85201",
+        "description": "A large park that features a splash pad, a 50-foot climbing tower, multiple slides, a large urban fishing lake, and shaded picnic ramadas."
+    },
+    {
+        "name": "Salt River",
+        "photo": "../images/salt-river.webp",
+        "address1": "9200 N. Bush Highway,",
+        "address2": "Mesa, AZ 85215",
+        "description": "The Salt River is a river in Gila and Maricopa counties in Arizona, United States, that is the largest tributary of the Gila River. The river is about 200 miles long. Its drainage basin covers about 13,700 square miles. The longest of the Salt River's many tributaries is the 195-mile Verde River."
+    },
+    {
+        "name": "Superstition Mountains",
+        "photo": "../images/superstition-mountains.webp",
+        "address1": "6109 N. Apache Trail,",
+        "address2": "Apache Junction, AZ 85119",
+        "description": "The Superstition Mountains is a range of mountains in Arizona located to the east of the Phoenix metropolitan area. They are anchored by Superstition Mountain, a large mountain that is a popular recreation destination for residents of the Phoenix, Arizona, area."
+    },
+    {
+        "name": "Usery Mountain Regional Park",
+        "photo": "../images/usery-mountain-park.webp",
+        "address1": "3939 N Usery Pass Rd,",
+        "address2": "Mesa, AZ 85207",
+        "description": "A 3,648-acre park offering camping, archery & 29 miles of multi-use trails plus scenic wind caves."
+    }
+];
+
+export default pointsOfInterest;
