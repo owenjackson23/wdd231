b8a68f176f3ce82924b335031882a49700e8a98c
owenjackson23 <owenryanjackson@icloud.com>
2026-06-14 16:42:53 -0700

    added photo credit

--- chamber/data/discover-data.mjs
+++ chamber/data/discover-data.mjs
@@ -2,6 +2,8 @@ const pointsOfInterest = [
     {
         "name": "Arizona Museum of Natural History",
         "photo": "../images/az-museum-of-natural-history.webp",
+        "photoCredit": "Bing Places for Business",
+        "creditLink": "https://bpprodstorage.blob.core.windows.net/bpblobcontainer/5413884/images/727ab8f1-e325-4997-a26f-5599b390b662m4fMG6ttdOa56lJa3k-T-VN1GYxViIBgP8dpZum3caeafwZt5zkC2jl_QCV5CBjISb_v_suwY9kFlmGsLw=s0.Jpeg",
         "address1": "53 N Macdonald,",
         "address2": "Mesa, AZ 85201",
         "description": "The Arizona Museum of Natural History located in Mesa, Arizona, is the only natural history museum in the greater Phoenix area. It exhibits the natural and cultural history of the Southwestern United States."
@@ -9,6 +11,8 @@ const pointsOfInterest = [
     {
         "name": "Canyon Lake",
         "photo": "../images/canyon-lake.webp",
+        "photoCredit": "James Wilson",
+        "creditLink": "https://www.pexels.com/@james-wilson-560941481/",
         "address1": "16802 AZ-88,",
         "address2": "Apache Junction, AZ 85119",
         "description": "Canyon Lake is one of four reservoirs that were formed by the damming of the Salt River in the U.S. state of Arizona as part of the Salt River Project. It was formed by the Mormon Flat Dam, which was completed in 1925 after two years of construction."
@@ -16,6 +20,8 @@ const pointsOfInterest = [
     {
         "name": "Mesa Arts Center",
         "photo": "../images/mesa-arts-center.webp",
+        "photoCredit": "Wikipedia",
+        "creditLink": "https://es.wikipedia.org/wiki/Centro_de_Artes_de_Mesa",
         "address1": "1 E. Main St.,",
         "address2": "Mesa, AZ 85201",
         "description": "The Mesa Arts Center is a performing and visual arts complex in downtown Mesa, Arizona. At more than 210,000 square feet square feet, the $95 million facility, completed in 2005, is the largest comprehensive arts campus in the state."
@@ -23,6 +29,8 @@ const pointsOfInterest = [
     {
         "name": "Organ Stop Pizza",
         "photo": "../images/organ-stop-pizza.webp",
+        "photoCredit": "Tripadvisor",
+        "creditLink": "https://www.tripadvisor.com/Attraction_Review-g31281-d4507436-Reviews-Organ_Stop_Pizza-Mesa_Arizona.html",
         "address1": "1149 E Southern Ave.,",
         "address2": "Mesa, AZ 85204",
         "description": "Organ Stop Pizza is a pizza restaurant that boasts live entertainment performed on a massive organ."
@@ -30,6 +38,8 @@ const pointsOfInterest = [
     {
         "name": "Riverview Park",
         "photo": "../images/riverview-park.webp",
+        "photoCredit": "Tripadvisor",
+        "creditLink": "https://www.tripadvisor.com/Attraction_Review-g31281-d6487399-Reviews-Riverview_Park-Mesa_Arizona.html",
         "address1": "2100 W Rio Salado Pkwy,",
         "address2": "Mesa, AZ 85201",
         "description": "A large park that features a splash pad, a 50-foot climbing tower, multiple slides, a large urban fishing lake, and shaded picnic ramadas."
@@ -37,6 +47,8 @@ const pointsOfInterest = [
     {
         "name": "Salt River",
         "photo": "../images/salt-river.webp",
+        "photoCredit": "Chuck Shields",
+        "creditLink": "https://www.pexels.com/@chuck-shields-1116201851/",
         "address1": "9200 N. Bush Highway,",
         "address2": "Mesa, AZ 85215",
         "description": "The Salt River is a river in Gila and Maricopa counties in Arizona, United States, that is the largest tributary of the Gila River. The river is about 200 miles long. Its drainage basin covers about 13,700 square miles. The longest of the Salt River's many tributaries is the 195-mile Verde River."
@@ -44,6 +56,8 @@ const pointsOfInterest = [
     {
         "name": "Superstition Mountains",
         "photo": "../images/superstition-mountains.webp",
+        "photoCredit": "Patrick Boyer",
+        "creditLink": "https://www.pexels.com/@patrick-boyer-2209232/",
         "address1": "6109 N. Apache Trail,",
         "address2": "Apache Junction, AZ 85119",
         "description": "The Superstition Mountains is a range of mountains in Arizona located to the east of the Phoenix metropolitan area. They are anchored by Superstition Mountain, a large mountain that is a popular recreation destination for residents of the Phoenix, Arizona, area."
@@ -51,6 +65,8 @@ const pointsOfInterest = [
     {
         "name": "Usery Mountain Regional Park",
         "photo": "../images/usery-mountain-park.webp",
+        "photoCredit": "Lindsey Willard",
+        "creditLink": "https://www.pexels.com/@creativelindseyphotos/",
         "address1": "3939 N Usery Pass Rd,",
         "address2": "Mesa, AZ 85207",
         "description": "A 3,648-acre park offering camping, archery & 29 miles of multi-use trails plus scenic wind caves."
