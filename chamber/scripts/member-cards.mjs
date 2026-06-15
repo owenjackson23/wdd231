929ccc61d44ae80182bf89b77d3b1df3c0325d6b
owenjackson23 <owenryanjackson@icloud.com>
2026-06-14 18:58:49 -0700

    made addresses address elements

--- chamber/scripts/member-cards.mjs
+++ chamber/scripts/member-cards.mjs
@@ -8,8 +8,7 @@ const displayMembers = (members) => {
         // Create elements for the card
         let card = document.createElement('section');
         let name = document.createElement('h2');
-        let address1 = document.createElement('p');
-        let address2 = document.createElement('p');
+        let address = document.createElement('address');
         let phone = document.createElement('p');
         let website = document.createElement('a');
         let visitWebsite = document.createElement('button');
@@ -23,10 +22,7 @@ const displayMembers = (members) => {
         logo.setAttribute('width', '150');
         logo.setAttribute('height', 'auto');
         name.innerHTML = member.name;
-        address1.innerHTML = member.address1;
-        address1.classList.add('address');
-        address2.innerHTML = member.address2;
-        address2.classList.add('address');
+        address.innerHTML = `${member.address1}<br>${member.address2}`;
         phone.innerHTML = member.phone;
         phone.classList.add('phone');
         website.setAttribute('href', member.website);
@@ -49,8 +45,7 @@ const displayMembers = (members) => {
         // Append elements to the card
         card.appendChild(name);
         card.appendChild(logo);
-        card.appendChild(address1);
-        card.appendChild(address2);
+        card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(level);
         card.appendChild(website);
