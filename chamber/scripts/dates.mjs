dd43ead4308ad6c3c4ae9ec3e1fd1b3f9894c9d8
owenjackson23 <owenryanjackson@icloud.com>
2026-06-15 00:48:42 -0700

    discover message js

--- chamber/scripts/dates.mjs
+++ chamber/scripts/dates.mjs
@@ -1,9 +1,49 @@
 const year = document.getElementById('currentYear');
 const lastMod = document.getElementById('lastModified');
 
+const msToDays = 86400000;
 const today = new Date();
 
 export function displayDates() {
     year.innerHTML = today.getFullYear();
     lastMod.innerHTML = document.lastModified;
+}
+
+// Returns the number of days since the last page visit
+export function getDaysSinceVisit() {
+    let message = ``;
+
+    // Retrieve last visited date
+    const visited = localStorage.getItem("lastVisit");
+
+    // If the page has never been visited,
+    //      displays welcome message
+    if (!visited) {
+        message = `Welcome! Let us know if you have any questions.`;
+        localStorage.setItem("lastVisit", new Date().toISOString());
+    }
+
+
+    else {
+        const lastVisit = new Date(visited);
+        console.log(lastVisit);
+        const now = new Date();
+
+        // Compare current date to last visit
+        const diffMs = now - lastVisit;
+        const diffDays = Math.floor(diffMs / msToDays);
+        console.log(diffDays);
+
+        // If the page was visited less than a day ago,
+        //      "Back so soon?" message is displayed
+        if (diffDays < 1) {
+            message = `Back so soon! Awesome!`;
+        }
+
+        // Display number of days passed
+        else {
+            message = `You last visited ${diffDays} days ago.`;
+        }
+    }
+    return message;
 }
--- chamber/scripts/discover.mjs
+++ chamber/scripts/discover.mjs
@@ -1,8 +1,11 @@
 import { setupHamButton } from './hamburger.mjs';
-import { displayDates } from './dates.mjs';
+import { displayDates, getDaysSinceVisit } from './dates.mjs';
 import { displayDiscoverCards } from './discover-cards.mjs';
 import pointsOfInterest from "../data/discover-data.mjs";
+
+const message = document.getElementById('message');
 
 setupHamButton();
 displayDates();
 displayDiscoverCards(pointsOfInterest);
+message.innerHTML = getDaysSinceVisit();
