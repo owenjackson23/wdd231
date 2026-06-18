ac6472017c13eb507f5051ea46af4c342144110e
owenjackson23 <owenryanjackson@icloud.com>
2026-06-18 02:53:21 -0700

    added destination images

--- final/scripts/destination-data.mjs
+++ final/scripts/destination-data.mjs
@@ -3,43 +3,43 @@ export const planets = [
         "name": "Tatooine",
         "number": 0,
         "activity": "Cheer for your favorite racer at the Mos Espa Podrace.",
-        "image": ""
+        "image": "images/canyon-dune-turn.webp"
     },
     {
         "name": "Alderaan",
         "number": 1,
         "activity": "Enjoy breathtaking views as you hike through the beautiful terrain.",
-        "image": ""
+        "image": "images/aldera-city.webp"
     },
     {
         "name": "Yavin IV",
         "number": 2,
         "activity": "Explore the secret rebel base built into the ruins of ancient Sith temples.",
-        "image": ""
+        "image": "images/yavin-4.webp"
     },
     {
         "name": "Hoth",
         "number": 3,
         "activity": "There is always fresh powder on Hoth's ski slopes.",
-        "image": ""
+        "image": "images/biomes-hoth.webp"
     },
     {
         "name": "Dagobah",
         "number": 4,
         "activity": "Enjoy the sounds of nature and see unique creatures as you camp in Dagobah's infamous swamps.",
-        "image": ""
+        "image": "images/gnarltree.webp"
     },
     {
         "name": "Bespin",
         "number": 5,
         "activity": "Take a cloud car for a joyride around Cloud City.",
-        "image": ""
+        "image": "images/cloud-city.webp"
     },
     {
         "name": "Endor",
         "number": 6,
         "activity": "Race through the dense forest on Imperial speeder bikes.",
-        "image": ""
+        "image": "images/endor.webp"
     },
     {
         "name": "Naboo",
@@ -51,31 +51,31 @@ export const planets = [
         "name": "Coruscant",
         "number": 8,
         "activity": "This city world is the perfect place for a shopping spree.",
-        "image": ""
+        "image": "images/coruscant.webp"
     },
     {
         "name": "Kamino",
         "number": 9,
         "activity": "Surf gigantic waves as you encounter familiar faces.",
-        "image": ""
+        "image": "images/kaminoan-rider.webp"
     },
     {
         "name": "Geonosis",
         "number": 10,
         "activity": "Explore massive underground catacombs. Our guides have committed every junction to memory.",
-        "image": ""
+        "image": "images/geonosis.webp"
     },
     {
         "name": "Mustafar",
         "number": 12,
         "activity": "Get up close and personal with some of the galaxy's largest active volcanoes.",
-        "image": ""
+        "image": "images/lava-rivers.webp"
     },
     {
         "name": "Kashyyyk",
         "number": 13,
         "activity": "Get a nice tan on the beach or get a legendary view from Kashyyyk's treetops.",
-        "image": ""
+        "image": "images/kashyyyk-staging-area.webp"
     }
     // {
     //     "name": "Corellia",
