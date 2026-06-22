0b4f49bbbf13c54ee5a7f78b55d40dbe0769f4a8
owenjackson23 <owenryanjackson@icloud.com>
2026-06-22 02:20:46 -0700

    modal added to booking

--- final/scripts/modal.mjs
+++ final/scripts/modal.mjs
@@ -1,8 +1,17 @@
 export const modal = document.getElementById('confirm-modal');
+const openModal = document.querySelector('.open-modal');
+const confirm = document.getElementById('confirm-modal');
 const closeModal = document.getElementById('close-modal');
 
 export function modalHandling() {
+    openModal.addEventListener('click', () => {
+        modal.showModal();
+    });
+
     closeModal.addEventListener('click', () => {
+        modal.close();
+    });
+    confirm.addEventListener('click', () => {
         modal.close();
     });
 }
