6096a550a49ea1c37dc5cc128114869e8ff5ddad
owenjackson23 <owenryanjackson@icloud.com>
2026-06-21 18:03:16 -0700

    updated modal.mjs

--- final/scripts/modal.mjs
+++ final/scripts/modal.mjs
@@ -1,5 +1,5 @@
-export const modal = document.getElementById('book-modal');
-const closeModal = document.getElementById('book-vacation');
+export const modal = document.getElementById('confirm-modal');
+const closeModal = document.getElementById('close-modal');
 
 export function modalHandling() {
     closeModal.addEventListener('click', () => {
