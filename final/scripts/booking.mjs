bbe55d4ec22da7381a9e470f1ab9e8fc6892a380
owenjackson23 <owenryanjackson@icloud.com>
2026-06-21 18:01:36 -0700

    setup booking.mjs

--- final/scripts/booking.mjs
+++ final/scripts/booking.mjs
@@ -1,8 +1,8 @@
 import { displayDates } from './dates.mjs';
 import { setupHamButton } from './hamburger.mjs';
-import { displaySpotlights } from './destination-cards.mjs';
+import { modalHandling } from './modal.mjs';
 
 displayDates();
 setupHamButton();
 
-displaySpotlights();
+modalHandling();
--- final/scripts/destinations.mjs
+++ final/scripts/destinations.mjs
@@ -1,10 +1,8 @@
 import { displayDates } from './dates.mjs';
 import { setupHamButton } from './hamburger.mjs';
 import { displayDestinations } from './destination-cards.mjs';
-import { modalHandling } from './modal.mjs';
 
 displayDates();
 setupHamButton();
 
 displayDestinations();
-modalHandling();
